afe0ce7faf5c320a525de7d2e81ccaba7c62e453
Pratyan Majumder <heypratyan@gmail.com>
2023-01-26 23:28:01 +0530

    Add files via upload

---
+++ src/views/About.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+// react-bootstrap components
+import {
+  Badge,
+  Button,
+  Card,
+  Navbar,
+  Nav,
+  Container,
+  Row,
+  Col,
+} from "react-bootstrap";
+
+function About() {
+  return (
+    <>
+      <Container fluid>
+        <Row>
+          <Col md="12">
+            <Card>
+              
+              <Card.Body>
+                
+                <div className="typography-line">
+                  <h4>
+                    
+                    Founded in India, TFT is a revolutionary cryptocurrency trading platform that has revolutionized the way people trade. It offers a wide range of services to its users, including zero brokerage fees, a pool to buy and sell USDT and INR directly from the company, and a loan system. This loan system allows users to take out loans for trading and personal use with minimal interest. 
+
+
+What sets TFT apart from other platforms is its introduction of TFF, its native token. When used in connection with taking out loans, users can enjoy significantly reduced interest rates, or even no interest at all. This makes TFT one of the best options for those looking for a secure and affordable way to trade and use cryptocurrencies. 
+
+
+TFT has implemented a range of security measures to ensure the safety of its users. The platform is secured by multiple layers of encryption, and all transactions are verified by cryptographically secure hash functions. Furthermore, all of the user data is stored in a secure, encrypted database, and users can rest assured that their private information will remain safe. The company also has a dedicated support team that is available 24/7 in case any issues arise or any questions need to be answered.
+
+
+The platform also offers a variety of features that make trading easier and more accessible for users. It has a user-friendly interface that allows users to quickly and efficiently access the services they need. Furthermore, users can also benefit from advanced charting tools, a range of trading options, and real-time market data. TFT also provides its users with a variety of educational resources to help them become better informed traders. 
+
+
+Overall, TFT is an excellent choice for anyone looking for a secure and convenient way to trade and use cryptocurrencies. With its cutting-edge technology, top-notch customer support, and a wide range of services, TFT is the perfect choice for anyone looking for a reliable and hassle-free cryptocurrency trading platform. It has everything that users need to make the most out of their cryptocurrency trading experience. With its vast array of features and services, TFT is sure to be a hit with both novice and experienced traders alike.
+                  </h4>
+                </div>
+                
+      
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+}
+
+export default About;
