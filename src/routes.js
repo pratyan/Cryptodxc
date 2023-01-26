319f6b61a686d81748ff9ca616a426cd2fe0df34
Pratyan Majumder <heypratyan@gmail.com>
2023-01-26 23:26:28 +0530

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,7 @@
 import Dashboard from "views/Dashboard.js";
 import UserProfile from "views/UserProfile.js";
 import TableList from "views/TableList.js";
-import Typography from "views/Typography.js";
-import Icons from "views/Icons.js";
-import Maps from "views/Maps.js";
+import About from "views/About.js";
 import Notifications from "views/Notifications.js";
 
 const dashboardRoutes = [
@@ -31,26 +29,14 @@ const dashboardRoutes = [
     layout: "/admin"
   },
   {
-    path: "/typography",
-    name: "Typography",
+    path: "/about",
+    name: "About",
     icon: "nc-icon nc-paper-2",
-    component: Typography,
+    component: About,
     layout: "/admin"
   },
-  {
-    path: "/icons",
-    name: "Icons",
-    icon: "nc-icon nc-atom",
-    component: Icons,
-    layout: "/admin"
-  },
-  {
-    path: "/maps",
-    name: "Maps",
-    icon: "nc-icon nc-pin-3",
-    component: Maps,
-    layout: "/admin"
-  },
+  
+  
   {
     path: "/notifications",
     name: "Notifications",
